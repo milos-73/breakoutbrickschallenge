1480a392377d54fca7268eb6f885928421f326e2
milos-73 <admin@viatatry.eu>
2023-11-17 20:41:56 +0100

    Pridaná celoobrazovková reklama po stlašení tlačidla back to menu; Implementácia podpori iOS zariadenia

--- lib/components/back_to_menu_button.dart
+++ lib/components/back_to_menu_button.dart
@@ -3,35 +3,68 @@ import 'dart:ui';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame_audio/flame_audio.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:initial_project/services/saved_values.dart';
 import '../forge2d_game_world.dart';
+import '../services/ad_helper.dart';
 
 class BackToMenuButton extends SpriteComponent with Tappable, HasGameRef<BrickBreakGame>{
 
+ static const int maxFailedLoadAttempts = 3;
+
  SavedValues savedValues = SavedValues();
+ InterstitialAd? _interstitialAd;
+ int _interstitialLoadAttempts = 1;
 
- @override
- Future<void> onLoad() async {
-  await super.onLoad();
-  sprite = await gameRef.loadSprite('buttons/home_button2.png');
-  position = Vector2(30 , 7.9);
-  size = Vector2(4, 4);
-  priority = 5;
+
+ void _createInterstitialAd() {
+  InterstitialAd.load(
+   adUnitId: AdHelper.interstitialAdUnitId,
+   request: AdRequest(),
+   adLoadCallback: InterstitialAdLoadCallback(
+    onAdLoaded: (InterstitialAd ad) {
+     _interstitialAd = ad;
+     _interstitialLoadAttempts = 0;
+    },
+    onAdFailedToLoad: (LoadAdError error) {
+     _interstitialLoadAttempts += 1;
+     _interstitialAd = null;
+     if (_interstitialLoadAttempts <= maxFailedLoadAttempts) {
+      _createInterstitialAd();
+     }
+    },
+   ),
+  );
  }
 
+ Future<void> _showInterstitialAd() async {
+  if (_interstitialAd != null) {
+   _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+    onAdDismissedFullScreenContent: (InterstitialAd ad) {
+     ad.dispose();
+     _overlays();
+     _createInterstitialAd();
+    },
+    onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+      ad.dispose();
 
- @override
- bool onTapUp(TapUpInfo info) {
-  print("tap up");
-  return false;
+     _createInterstitialAd();
+    },
+   );
+
+   await _interstitialAd!.show();
+
+  }
  }
 
- @override
- bool onTapDown(TapDownInfo info) {
+ Future<void> _overlays() async {
 
-  print('GAME STATE: ${gameRef.gameState}');
+
   if (game.audioSettings == AudioSettings.on)  {
-  FlameAudio.play('button3.mp3');}
+   FlameAudio.play('button3.mp3');}
+
+
+
   savedValues.saveLevelInProgressNumber(gameRef.currentPlayedLevelNumber);
 
   if (gameRef.overlays.isActive('PreGame')){gameRef.overlays.remove('PreGame');}
@@ -47,7 +80,59 @@ class BackToMenuButton extends SpriteComponent with Tappable, HasGameRef<BrickBr
 
   gameRef.pauseEngine();
   gameRef.gameState = GameState.paused;
+
+
+
   gameRef.overlays.add('MainMenu');
+
+}
+
+
+
+
+
+ @override
+ Future<void> onLoad() async {
+  await super.onLoad();
+  _createInterstitialAd();
+  sprite = await gameRef.loadSprite('buttons/home_button2.png');
+  position = Vector2(30 , 7.9);
+  size = Vector2(4, 4);
+  priority = 5;
+ }
+
+ @override
+ bool onTapUp(TapUpInfo info) {
+  print("tap up");
+  return false;
+ }
+
+ @override
+ bool onTapDown(TapDownInfo info) {
+
+  _showInterstitialAd();
+
+
+  print('GAME STATE: ${gameRef.gameState}');
+  // if (game.audioSettings == AudioSettings.on)  {
+  // FlameAudio.play('button3.mp3');}
+  // savedValues.saveLevelInProgressNumber(gameRef.currentPlayedLevelNumber);
+  //
+  // if (gameRef.overlays.isActive('PreGame')){gameRef.overlays.remove('PreGame');}
+  // if (gameRef.overlays.isActive('LostLife')){gameRef.overlays.remove('LostLife');}
+  // if (gameRef.overlays.isActive('GamePausedMenuOverlay')){gameRef.overlays.remove('GamePausedMenuOverlay');}
+  // if (gameRef.overlays.isActive('ChallengeGameOverOverlay')){gameRef.overlays.remove('ChallengeGameOverOverlay');}
+  // if (gameRef.overlays.isActive('NextLevelChallengeModeOverlay')){gameRef.overlays.remove('NextLevelChallengeModeOverlay');}
+  // if (gameRef.overlays.isActive('WinGame')){gameRef.overlays.remove('WinGame');}
+  // if (gameRef.overlays.isActive('LostLife')){gameRef.overlays.remove('LostLife');}
+  //
+  // if(gameRef.gameMode == GameMode.challenge){gameRef.fallingPoints?.resetFallingPoints();}
+  // if(gameRef.gameMode == GameMode.levels){gameRef.fallingStars?.resetFallingStars();}
+  //
+  // gameRef.pauseEngine();
+  // gameRef.gameState = GameState.paused;
+  //
+  // gameRef.overlays.add('MainMenu');
 
   info.handled = true;
   return true;
